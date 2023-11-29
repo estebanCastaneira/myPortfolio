c1181ac72d8af23a3a0dec9528c107681bc5dbf9
estebanCastaneira <estebanc_b@hotmail.com>
2023-11-29 14:11:35 -0300

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 className="text-red-400 font-breez-sans font-thin">va pa e</h1>
-      <h1 className="text-red-400 font-breez-sans font-light">va pa e</h1>
-      <h1 className="text-red-400 font-breez-sans">va pa e</h1>
-      <h1 className="text-red-400 font-breez-sans font-medium">va pa e</h1>
-      <h1 className="text-red-400 font-breez-sans font-bold">va pa e</h1>
+      <h1 className=" font-breez-sans font-thin">va pa e</h1>
     </div>
   )
 }
