6c1c03f05344259da2ab8a544554fa3f523945bd
estebanCastaneira <estebanc_b@hotmail.com>
2023-12-01 16:00:37 -0300

    Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,12 @@
-import LinkedinIcon from "../assets/icons/LinkedinIcon"
-import GithubIcon from "../assets/icons/GithubIcon"
-import neonText from "../functions/neonText"
+import Dropdown from "./Dropdown"
+import LinkedinIcon from "../../assets/icons/LinkedinIcon"
+import GithubIcon from "../../assets/icons/GithubIcon"
+import neonText from "../../functions/neonText"
 function Header() {
   return (
     <header className="bg-zinc-800 py-4 sticky top-0 backdrop-filter backdrop-blur-lg bg-opacity-10">
       <nav>
-        <div className="w-[85%] my-0 mx-auto flex justify-between items-center sm:items-start ">
+        <div className="w-[80%] my-0 mx-auto flex justify-between items-center sm:items-start ">
           <div className="flex flex-col items-start justify-center">
             <p className="font-bold mr-10">
               Esteban Castañeira
@@ -37,42 +38,18 @@ function Header() {
           </div>
           <div>
             <ul className="sm:flex hidden gap-9 ">
-              <li>
+              <li className="hover:scale-90 transform transition">
                 <a href="#">About</a>
               </li>
-              <li>
+              <li className="hover:scale-90 transform transition">
                 <a href="#">Portfolio</a>
               </li>
-              <li>
+              <li className="hover:scale-90 transform transition">
                 <a href="#">Contact</a>
               </li>
             </ul>
+            <Dropdown />
           </div>
-          {/* Hamburger Icon */}
-          <button
-            className="block border-0 bg-transparent px-2 text-white focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 dark:text-neutral-200 sm:hidden"
-            type="button"
-            data-te-collapse-init
-            data-te-target="#navbarSupportedContent1"
-            aria-controls="navbarSupportedContent1"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="[&>svg]:w-7">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="h-7 w-7"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </span>
-          </button>
         </div>
       </nav>
     </header>
