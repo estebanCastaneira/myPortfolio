c216e7912a57cbe636e10b48bb25d9326d922081
estebanCastaneira <estebanc_b@hotmail.com>
2023-12-01 16:00:08 -0300

    Dropdown menu works, styles fixes

---
+++ src/components/header/Dropdown.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react"
+
+function Dropdown() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleOnClick = () => {
+    setIsOpen(!isOpen)
+  }
+  useEffect(() => {
+    const screenW = window.visualViewport.width
+    screenW > 647 && setIsOpen(false)
+  }, [])
+  return (
+    <div className="relative flex flex-col items-center">
+      <button
+        className="block border-transparent active:border-white duration-300 bg-transparent p-2 text-white focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 dark:text-neutral-200 sm:hidden"
+        type="button"
+        data-te-collapse-init
+        data-te-target="#navbarSupportedContent1"
+        aria-controls="navbarSupportedContent1"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+        onClick={handleOnClick}
+      >
+        <span className="[&>svg]:w-7">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="h-7 w-7"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </span>
+      </button>
+      {isOpen && (
+        <div className="bg-zinc-800 bg-opacity-20 absolute top-20 rounded-lg border border-zinc-700 p-3">
+          <ul className="flex flex-col items-center gap-6 ">
+            <li className="flex justify-center hover:brightness-105 px-4 py-1 w-full">
+              <a href="#">About</a>
+            </li>
+            <li className="flex justify-center hover:brightness-105  px-4 py-1 w-full">
+              <a href="#">Portfolio</a>
+            </li>
+            <li className="flex justify-center hover:brightness-105  px-4 py-1 w-full">
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  )
+}
+export default Dropdown
