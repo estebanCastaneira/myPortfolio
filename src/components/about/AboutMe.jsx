a13d6c6e96ddac97d65420d642a25b55dd8487ab
estebanCastaneira <estebanc_b@hotmail.com>
2023-12-18 11:51:51 -0300

    broken hero video fixes (suppose)

--- src/components/about/AboutMe.jsx
+++ src/components/about/AboutMe.jsx
@@ -2,8 +2,8 @@ import me from "/img/me.jpg"
 function AboutMe() {
   return (
     <div>
-      <h3 className="neonText text-2xl sm:text-4xl text-center tracking-widest">
-        About <span className="flicker">M</span>e
+      <h3 className="neonText text-4xl sm:text-6xl text-center tracking-widest">
+        <span className="flicker">M</span>e
       </h3>
       <div className="flex flex-col sm:flex-row gap-10 justify-center items-center mt-10">
         <div className="container-profile sm:w-[50%] ">
