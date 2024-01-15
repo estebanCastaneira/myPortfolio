258ef4441cf45fdeac990d205535baa48953ffdb
estebanCastaneira <estebanc_b@hotmail.com>
2024-01-15 09:05:09 -0300

    year only checks once

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,15 +1,19 @@
 import ContactLine from "../contactline/ContactLine"
+import { useEffect, useState } from "react"
 function Footer() {
-  const year = new Date().getFullYear()
+  const [year, setYear] = useState()
+  useEffect(() => {
+    const currentYear = new Date().getFullYear()
+    setYear(currentYear)
+  }, [])
+
   return (
     <footer className="bg-zinc-900 py-10">
-      <div className="w-[80%] my-0 mx-auto flex flex-col justify-end items-center gap-2 relative">
-        <p className="font-thin text-lg  whitespace-nowrap">
+      <div className="w-[80%] my-0 mx-auto text-lg flex flex-col justify-end items-center gap-2">
+        <p className="font-thin whitespace-nowrap">
           © Copyright {year} Esteban Castañeira.
         </p>
-        <div className="flex flex-col gap-2">
-          <p className="font-thin text-lg">Montevideo, Uruguay 🧉 🇺🇾</p>
-        </div>
+        <p className="font-thin">Montevideo, Uruguay 🧉 🇺🇾</p>
       </div>
     </footer>
   )
