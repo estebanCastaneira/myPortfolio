613664c1bee8a95c7c0ef289778528f7d2bbf373
estebanCastaneira <estebanc_b@hotmail.com>
2024-01-15 09:00:52 -0300

    footer items center

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,12 +3,12 @@ function Footer() {
   const year = new Date().getFullYear()
   return (
     <footer className="bg-zinc-900 py-10">
-      <div className="w-[80%] my-0 mx-auto flex flex-col sm:justify-end sm:items-center gap-2 relative">
+      <div className="w-[80%] my-0 mx-auto flex flex-col justify-end items-center gap-2 relative">
         <p className="font-thin text-lg  whitespace-nowrap">
           © Copyright {year} Esteban Castañeira.
         </p>
         <div className="flex flex-col gap-2">
-          <p className="font-thin text-lg">Montevideo, Uruguay 🧉 🇺🇾.</p>
+          <p className="font-thin text-lg">Montevideo, Uruguay 🧉 🇺🇾</p>
         </div>
       </div>
     </footer>
